387fd6497fcadfb1f0c76551f201b86a0cc3b430
Hilmar Wiegand <me@hwgnd.de>
2021-12-02 23:36:43 +0100

    Add project files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Advent of Code 2021 in Elixir
 
-To execute the scripts, run `iex -r util.ex` to get a prompt with the util module loaded, and then use `c "dayXX.exs"` to run each individual solution.
+Use `mix run lib/dayXX.exs` to run each solution.
---
+++ mix.exs
@@ -0,0 +1,15 @@
+defmodule Advent2021.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent2021,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      deps: deps()
+    ]
+  end
+
+  def application, do: []
+  defp deps, do: []
+end
